baf5836774d5ebf221d3bdc3aeff33467cb2454b
Marcin Bigos <marcin_99999@interia.pl>
2018-12-06 21:15:02 +0100

    improvement

--- src/RoboChat.Library/Settings/MongoConfigurator.cs
+++ src/RoboChat.Library/Settings/MongoConfigurator.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 
-namespace RoboChat.Library
+namespace RoboChat.Library.Settings
 {
     public static class MongoConfigurator
     {
@@ -16,8 +11,7 @@ namespace RoboChat.Library
         
         public static IMongoDatabase Connect()
         {
-            //var connectionString = new MongoUrl("mongodb://chathistoryuser:eaugfb43eubagfb431naenfaei@ds237120.mlab.com:37120/robochat");
-            var connectionString = new MongoUrl("mongodb://user:user123@ds225703.mlab.com:25703/trackmymoney");
+            var connectionString = new MongoUrl("");
             MongoClient client = new MongoClient(connectionString.ToString());
             return client.GetDatabase(connectionString.DatabaseName);
         }
